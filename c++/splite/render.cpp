cbd7f55a7856311f5394b4595f67323fc3822151
Mark Harris <mharris@technobear.com>
2019-04-18 22:48:17 +0200

    start more flexible splite

--- c++/splite/render.cpp
+++ c++/splite/render.cpp
@@ -12,17 +12,15 @@ std::chrono::time_point<std::chrono::system_clock>  gLastErrorTime;
 
 #include <iostream>
 
-struct SPTouch {
+struct SPZone {
+	SPZone() : id_(0), active_(false), x_(0.0f),y_(0.0f),z_(0.0f), pitch_(0.0f) {;}
+	void reset() { id_=0;active_=false;x_=0.0f;y_ = 0.0f;z_ = 0.0f; pitch_=0.0f;}
+	
 	unsigned id_;
 	bool active_;
 	float x_,y_,z_;
 	float pitch_;
-} ;
-
-SPTouch lSlider_ 	{0,false,0.0f,0.0f,0.0f,0.0f};
-SPTouch lPanel_  	{0,false,0.0f,0.0f,0.0f,0.0f};
-SPTouch rPanel_ 	{0,false,0.0f,0.0f,0.0f,0.0f};
-SPTouch rSlider_ 	{0,false,0.0f,0.0f,0.0f,0.0f};
+} zone_[4];
 
 #ifdef SALT
 	#define OUT_VOLT_RANGE 10.0f
@@ -31,6 +29,54 @@ SPTouch rSlider_ 	{0,false,0.0f,0.0f,0.0f,0.0f};
 	#define OUT_VOLT_RANGE 5.0f
 	#define OUT_OFFSET 0.0f
 #endif 
+
+class ZoneMode {
+	virtual void updateTouch(unsigned  tId, bool active,float x, float y, float z);
+}
+
+class 1ZoneMode : public ZoneMode {
+	void updateTouch(unsigned  tId, bool active,float x, float y, float z) override {
+        	zone[1]_.id_ = tId;
+        	zone[1]_.active_ = active;
+        	zone[1]_.x_ = x  / 10.0f ;
+        	zone[1]_.y_ = y / 5.0f;
+        	zone[1]_.z_ = ((z / 2.0) * 2.0f)  + OUT_OFFSET;
+        	zone[1]_.pitch_ =  (x * semiMult) + OUT_OFFSET;
+	}
+}
+
+class 4ZoneMode : public ZoneMode {
+	void updateTouch(unsigned  tId, bool active,float x, float y, float z) override {
+        if(x < 2.0f) {
+        	zone[0]_.id_ = tId;
+        	zone[0]_.active_ = active;
+        	zone[0]_.y_ = y / 5.0f;
+        } else if(x < 12.0f) {
+        	zone[1]_.id_ = tId;
+        	zone[1]_.active_ = active;
+        	zone[1]_.x_ = (x - 2.0f)  / 10.0f ;
+        	zone[1]_.y_ = y / 5.0f;
+        	zone[1]_.z_ = ((z / 2.0) *2.0f)  + OUT_OFFSET;
+        	zone[1]_.pitch_ =  ((x - 2.0f)  * semiMult) + OUT_OFFSET;
+        } else if(x < 28.0f) {
+        	zone[2]_.id_ = tId;
+        	zone[2]_.active_ = active;
+        	zone[2]_.x_ = (x - 12.0f)  / 16.0f;
+        	zone[2]_.y_ = y / 5.0f;
+        	zone[2]_.z_ = ((z / 2.0) * 2.0f) + OUT_OFFSET;
+        	zone[2]_.pitch_ =  ((x - 12.0f)  * semiMult) + OUT_OFFSET;
+        } else {
+        	zone[3]_.id_ = tId;
+        	zone[3]_.active_ = active;
+        	zone[3]_.y_ = y / 5.0f;
+        }
+	}
+}
+
+ZoneMode* mode_[2] = {
+	new 1ZoneMode(),
+	new 4ZoneMode()
+};
 
 class BelaSPCallback : public SPLiteCallback {
 public:
@@ -61,30 +107,9 @@ public:
 private:
 	static constexpr float semiMult = (1.0f / (OUT_VOLT_RANGE * 12.0f)); // 1.0 = 10v = 10 octaves 
 	void updateTouch(unsigned  tId, bool active,float x, float y, float z) {
-        if(x < 2.0f) {
-        	lSlider_.id_ = tId;
-        	lSlider_.active_ = active;
-        	lSlider_.y_ = y / 5.0f;
-        } else if(x < 12.0f) {
-        	lPanel_.id_ = tId;
-        	lPanel_.active_ = active;
-        	lPanel_.x_ = (x - 2.0f)  / 10.0f ;
-        	lPanel_.y_ = y / 5.0f;
-        	lPanel_.z_ = ((z / 2.0) *2.0f)  + OUT_OFFSET;
-        	lPanel_.pitch_ =  ((x - 2.0f)  * semiMult) + OUT_OFFSET;
-        } else if(x < 28.0f) {
-        	rPanel_.id_ = tId;
-        	rPanel_.active_ = active;
-        	rPanel_.x_ = (x - 12.0f)  / 16.0f;
-        	rPanel_.y_ = y / 5.0f;
-        	rPanel_.z_ = ((z / 2.0) * 2.0f) + OUT_OFFSET;
-        	rPanel_.pitch_ =  ((x - 12.0f)  * semiMult) + OUT_OFFSET;
-        } else {
-        	rSlider_.id_ = tId;
-        	rSlider_.active_ = active;
-        	rSlider_.y_ = y / 5.0f;
-        }
+		mode_[modeIdx_]->updateTouch(tId,active,x,z,y);
 	}
+	unsigned modeIdx_;	
 
 };
 
@@ -136,22 +161,22 @@ void render(BelaContext *context, void *userData)
 	// }
 	
 	for(unsigned int n = 0; n < context->digitalFrames; n++) {
-		if(0  < context->digitalChannels) { digitalWriteOnce(context, n, 0,  lSlider_.active_);}	// trig1
-		if(5  < context->digitalChannels) { digitalWriteOnce(context, n, 5,  lPanel_.active_);}	// trig2
-		if(12  < context->digitalChannels) { digitalWriteOnce(context, n, 12,  rPanel_.active_);}	// trig3
-		if(13  < context->digitalChannels) { digitalWriteOnce(context, n, 13,  rSlider_.active_);}	// trig4
+		if(0  < context->digitalChannels) { digitalWriteOnce(context, n, 0,    zone[0].active_);}	// trig1
+		if(5  < context->digitalChannels) { digitalWriteOnce(context, n, 5,    zone[1].active_);}	// trig2
+		if(12  < context->digitalChannels) { digitalWriteOnce(context, n, 12,  zone[2].active_);}	// trig3
+		if(13  < context->digitalChannels) { digitalWriteOnce(context, n, 13,  zone[3].active_);}	// trig4
 	}
 
 	for(unsigned int n = 0; n < context->analogFrames; n++) {
 		unsigned ch = 0;
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, lSlider_.y_); ch++; }		// CV 1
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, lPanel_.pitch_ ); ch++; }	// CV 2
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, lPanel_.y_ ); ch++; } 	// CV 3
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, lPanel_.z_ ); ch++; } 	// CV 4
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, rPanel_.pitch_ ); ch++; } // CV 5
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, rPanel_.y_ ); ch++; }		// CV 6
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, rPanel_.z_ ); ch++; } 	// CV 7
-		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, rSlider_.y_); ch++; } 	// CV 8
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, zone[0].y_); ch++; }		// CV 1
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, zone[1].pitch_ ); ch++; }	// CV 2
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, zone[1].y_ ); ch++; } 	// CV 3
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, zone[1].z_ ); ch++; } 	// CV 4
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, zone[2].pitch_ ); ch++; } // CV 5
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, zone[2].y_ ); ch++; }		// CV 6
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, zone[2].z_ ); ch++; } 	// CV 7
+		if(ch < context->analogOutChannels) { analogWriteOnce(context, n, ch, zone[3].y_); ch++; } 	// CV 8
 	}
 
 }
